35a7f43d81653480cb03d30d4480d5e52ca7ec08
gautamcse27 <khushi27gkcse@gmail.com>
2024-11-29 16:54:19 +0530

    initial commit

---
+++ app/src/main/java/com/example/bhojpur_tourism5/MainActivity.kt
@@ -0,0 +1,61 @@
+package com.example.bhojpur_tourism5
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.os.PersistableBundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.drawerlayout.widget.DrawerLayout
+import com.example.bhojpur_tourism5.databinding.ActivityMainBinding
+import com.google.android.material.navigation.NavigationView
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var toggle : ActionBarDrawerToggle
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val drawerLayout : DrawerLayout = findViewById(R.id.drawerLayout)
+        val navView : NavigationView = findViewById(R.id.nav_view)
+
+        toggle = ActionBarDrawerToggle(this, drawerLayout,R.string.open,R.string.close)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        navView.setNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.nav_home -> Toast.makeText(applicationContext,  "Clicked Home", Toast.LENGTH_SHORT).show()
+                R.id.nav_message -> Toast.makeText(applicationContext,  "Clicked Message", Toast.LENGTH_SHORT).show()
+                R.id.nav_sync -> Toast.makeText(applicationContext,  "Clicked Sync", Toast.LENGTH_SHORT).show()
+                R.id.nav_trash -> Toast.makeText(applicationContext,  "Clicked Trash", Toast.LENGTH_SHORT).show()
+                R.id.nav_settings -> Toast.makeText(applicationContext,  "Clicked Settings", Toast.LENGTH_SHORT).show()
+                R.id.nav_share -> Toast.makeText(applicationContext,  "Clicked Share", Toast.LENGTH_SHORT).show()
+                R.id.nav_rate_us -> Toast.makeText(applicationContext,  "Clicked Rate us", Toast.LENGTH_SHORT).show()
+
+            }
+            true
+        }
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (toggle.onOptionsItemSelected(item)){
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+}
